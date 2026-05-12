ebcafa135e644116eedb39bbb592610c63ca3eda
Sushant Khadka <thesushantkhadka@gmail.com>
2026-05-11 22:34:16 -0700

    Created the loop to initialize the elements into the array and improved on the instructions

--- README.md
+++ README.md
@@ -18,6 +18,7 @@ At the beginning of the program, the calculator will display a title nad a short
 ```
 SMC RPN Calculator
 Enter 0-9 or +, -, *, /, or . to display result on TOS.
+It will accept up to 4 numeric digits and perform operations.
 ```
 ## Conditions
 ### Input
--- simple_math_calculator.asm
+++ simple_math_calculator.asm
@@ -10,7 +10,7 @@
 ; If stack is empty, operation will fail and program will reset to the beginning
 
 ; Algorithm
-; 1. Initialize RPN stack and test values 0, 9, +, -, *, /, and . 
+; 1. Initialize RPN stack and test values 0 - 9, +, -, *, /, and . 
 ; 2. Print title and instructions
 ; 3. Read character from keyboard
 ; 4. If operator, load operator vector, check stack for two numbers 
@@ -28,23 +28,47 @@
 
 ; Prompts:
 ;   > - ready to accept a new number
-;   ? - error occured, input doens't make sense
+;   ? - error occured, input doesn't make sense
 ;   $ - stack error occurred
 ;   ! - numeric overflow or underflow
 
 
 .orig x3000
-START       lea R4, count
-            lea R0, MSG1
+START       lea R4, count       ; RPN Stack Pointer Address
+            lea R0, MSG1        ; Printing Title and Instruction
             puts
             lea R0, MSG2
             puts
+            lea R0, MSG3
+            puts
 
-LOOP        lea R0, NEWNUM
+LOOP        lea R0, NEWNUM      ; Asking for a New Number/Operator
             puts
             getc
             lea R0, NEWLINE
             out
+            
+            ld R1, MULT         ; Setting R1 to "*" and turning it to its negative decimal value
+            not R1, R1
+            add R1, R1, #1
+            add R2, R0, R1      ; If R2 is negative, that means the input is not a valid input (decimal value is too small)
+            brn ERROR
+            
+            ld R1, ASCII_0      ; Setting R1 to ASCII 0 and turning it to its negative decimal value
+            not R1, R1
+            add R1, R1, #1
+            add R2, R0, R1      ; Since we checked the lower bound, anything less than ASCII 0 will be either a valid operator or ","
+            brn CHECK_OP
+            add R2, R2, #-9     ; If R2 is negative, that means the input is not a valid input (decimal value is too large)
+            brn ERROR
+            
+            ld R1, ASCII_0      ; Resetting R1 to ASCII 0 and turning it to its negative decimal value
+            not R1, R1
+            add R1, R1, #1
+            add R0, R0, R1      ; Converting the ASCII digit to an integer
+            str R0, R4, #0      ; Storing the integer in R4 (RPN Stack Pointer)
+            add R4, R4, #1      ; Incrementing the R4 and repeating the Loop until CHECK_OP notices a "." or an error occurs
+            brnzp LOOP
             halt
             
 result      .blkw 1
@@ -57,6 +81,7 @@ count       .blkw #8
 
 MSG1        .stringz "SMC RPN Calculator\n"
 MSG2        .stringz "Enter 0-9 or +, -, *, /, or . to display result on TOS.\n"
+MSG3        .stringz "It will accept up to 4 numeric digits and perform operations.\n"
 NEWNUM      .stringz "> "
 INPUTERROR  .stringz "? "
 STACKERROR  .stringz "$ "
